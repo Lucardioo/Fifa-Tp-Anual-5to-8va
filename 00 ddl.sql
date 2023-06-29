333755fc9fad83ee3a4d1d6f24855e5784b104f7
Lucardioo <lucas.godoy.et12@gmail.com>
2023-06-29 11:07:43 -0300

    DER + DATABASE

--- 00 ddl.sql
+++ 00 ddl.sql
@@ -4,55 +4,65 @@ CREATE DATABASE FIFA-TRANSACCIONES;
 
 USE FIFA-TRANSACCIONES;
 
-CREATE TABLE Posicion --falta las uniones
+CREATE TABLE Posicion -- no tiene uniones
 (
-    idPosicion  TINYINT     NOT NULL,
-    nombre      VARCHAR(45) NOT NULL,
-    numero      TINYINT     NOT NULL
+    idPosicion          TINYINT     NOT NULL,
+    nombre              VARCHAR(45) NOT NULL,
+    PRIMARY KEY (idPosicion)
 );
 
-CREATE TABLE Habilidad --falta las uniones
+CREATE TABLE Habilidad -- no tiene uniones
 (
-    idHabilidades   TINYINT     NOT NULL,
-    descripcion     VARCHAR(45) NOT NULL
+    idHabilidad       TINYINT     NOT NULL,
+    descripcion         VARCHAR(45) NOT NULL,
+    PRIMARY KEY (idhabilidad)
+    constraint idhabilidad Foreign Key (idhabilidad) REFERENCES Futbolista-Habilidad (idhabilidad)
+
 );
 
+CREATE TABLE Futbolista-Habilidad
+(
+    idFutbolista        TINYINT NOT NULL,
+    idHabilidad         TINYINT NOT NULL
+
+    PRIMARY KEY (idFutbolista, idHabilidad)
+);
 CREATE TABLE Futbolista
 (
     idFutbolista        TINYINT NOT NULL,
     idPosicion          TINYINT NOT NULL,
-    idHabilidades       TINYINT NOT NULL,
-    idjugador           TINYINT NOT NULL,
     nombre              VARCHAR(45),
     apellido            VARCHAR(45),
     fechaDeNacimiento   DATE    NOT NULL,
     velocidad           INT     NOT NULL,
     remate              INT     NOT NULL,
     pase                INT     NOT NULL,
-    defensa             INT     NOT NULL
+    defensa             INT     NOT NULL,
 
     PRIMARY KEY (idFutbolista)
-    CONSTRAINT Posicion FOREIGN 
+    CONSTRAINT idFutbolista FOREIGN KEY (idfutbolista) REFERENCES idfutbolista (Futbolista-Habilidad)
 );
 
 CREATE TABLE Jugador --falta las uniones
 (
-    idJugador       TINYINT NOT NULL,
-    nombre          VARCHAR(45) NOT NULL,
-    apellido        VARCHAR(45) NOT NULL,
-    nombreDeUsuario VARCHAR(15) NOT NULL,
-    contrasena      CHAR(64)    NOT NULL,
-    monedas         MEDIUMINT   NOT NULL,
+    idJugador           TINYINT NOT NULL,
+    nombre              VARCHAR(45) NOT NULL,
+    apellido            VARCHAR(45) NOT NULL,
+    nombreDeUsuario     VARCHAR(15) NOT NULL,
+    contrasena          CHAR(64)    NOT NULL,
+    monedas             MEDIUMINT   NOT NULL,
 
-    PRIMARY KEY (idJugador)
-    CONSTRAINT 
+    PRIMARY KEY (idJugador),
+    CONSTRAINT idjugador Foreign Key (idjugador) REFERENCES posesion (idjugador)
 );
 
 CREATE TABLE Posesion
 (
-    idJugador       TINYINT NOT NULL,
-    idFutbolista    TINYINT NOT NULL,
-    adquirido       BOOLEAN NOT NULL,
+    idJugador           TINYINT NOT NULL,
+    idFutbolista        TINYINT NOT NULL,
+    adquirido           BOOLEAN NOT NULL,
+
+    PRIMARY KEY (idJugador, IdFutbolista)
 );
 
 CREATE TABLE Transferencia
@@ -62,5 +72,7 @@ CREATE TABLE Transferencia
     fechaHora           DATETIME    NOT NULL,
     precio              MEDIUMINT   NOT NULL,
     monedas             MEDIUMINT   NOT NULL,
-    fechaConcentracion  DATE        NOT NULL
+    fechaConcentracion  DATE        NOT NULL,
+
+    PRIMARY KEY (idTransferencia)
 );
